72d7864ad617d16e60d6c027c2a1d7e5bd672884
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-26 22:01:49 +0300

    Update InterfaceCreator.java

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -2,6 +2,7 @@ package com.epam.mjc;
 
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
